943f8eda72139c6907e5931c17bcdfc844d9388b
Victor H. C. Silva <victorh.csilva20@gmail.com>
2026-08-29 12:23:56 -0300

    feat: migrando de bulma css para Tailwind CSS

--- src/core/urls.py
+++ src/core/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="main:login", permanent=False)),
     path("admin/", admin.site.urls),
     path("main/", include("main.urls")),
     path("agenda/", include("agenda.urls")),
